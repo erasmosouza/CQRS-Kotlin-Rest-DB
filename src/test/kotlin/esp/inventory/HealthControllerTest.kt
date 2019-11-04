5384ff2c7ec32bd15dafd4112e9f9ae7192dbf6a
erasmo.pinheiro <erasmosouza@gmail.com>
2019-11-04 08:48:35 -0300

    Inclusão de testes para o HealthCheck

---
+++ src/test/kotlin/esp/inventory/HealthControllerTest.kt
@@ -0,0 +1,33 @@
+package esp.inventory
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HealthControllerTest {
+
+    @LocalServerPort
+    var port: Int? = null
+
+    @Autowired
+    var restTemplate: TestRestTemplate? = null
+
+    @Test
+    fun healthRestTest() {
+
+        var health: Health? =
+            restTemplate?.getForObject("${getServerUrl()}/check?name=Kotlin", Health::class.java, 200)
+
+        Assert.assertEquals("Hello, Kotlin", health?.content)
+    }
+
+    fun getServerUrl(): String = "http://localhost:$port/"
+
+}
