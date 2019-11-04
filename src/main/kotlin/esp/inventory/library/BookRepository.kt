7d7f463d342c3b38e85829712675ee401c77ec8e
erasmo.pinheiro <erasmosouza@gmail.com>
2019-11-04 09:47:35 -0300

    Inclusão do BookRepository e os testes unitarios

---
+++ src/main/kotlin/esp/inventory/library/BookRepository.kt
@@ -0,0 +1,8 @@
+package esp.inventory.library
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository: JpaRepository<BookEntity, Long> {
+}
---
+++ src/test/kotlin/esp/inventory/library/BookRepositoryTest.kt
@@ -0,0 +1,78 @@
+package esp.inventory.library
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@DataJpaTest
+class BookRepositoryTest {
+
+    @Autowired
+    private var repository: BookRepository? = null
+
+    @Autowired
+    private var categoryRepository: CategoryRepository? = null
+
+    @Test
+    fun saveTest() {
+        val category: CategoryEntity = categoryRepository!!.save(CategoryEntity(categoryName = "Primeira Categoria"))
+        Assert.assertNotEquals(0L, category.id)
+
+        val book: BookEntity = repository!!.save(
+            BookEntity(
+                title = "Meu Livro",
+                author = "Erasmo Pinheiro",
+                description = "Descrição do meu primeiro livro",
+                category = category
+            )
+        )
+        Assert.assertNotEquals(0L, book.id)
+    }
+
+    @Test
+    fun updateTest(){
+        val category = categoryRepository!!.save(CategoryEntity(categoryName = "Primeira Categoria"))
+
+        Assert.assertNotEquals(0L, category.id)
+
+        val book: BookEntity = repository!!.save(
+            BookEntity(
+                title = "Meu Livro",
+                author = "Erasmo Pinheiro",
+                description = "Descrição do meu primeiro livro",
+                category = category
+            )
+        )
+        val id = book.id
+        Assert.assertNotEquals(0L, id)
+
+        book.title = "Meu Livro Updated"
+        val bookUpdated = repository!!.save(book)
+
+        Assert.assertEquals("Meu Livro Updated", bookUpdated.title)
+        Assert.assertEquals(id, bookUpdated.id)
+    }
+
+    @Test
+    fun deleteTest(){
+        val category = categoryRepository!!.save(CategoryEntity(categoryName = "Primeira Categoria"))
+        val book: BookEntity = repository!!.save(
+            BookEntity(
+                title = "Meu Livro",
+                author = "Erasmo Pinheiro",
+                description = "Descrição do meu primeiro livro",
+                category = category
+            )
+        )
+        val id = book.id
+
+        repository!!.deleteById(id)
+
+        val oldBook = repository!!.findById(id)
+        Assert.assertEquals( true, oldBook.isEmpty)
+    }
+}
