1564e2d49965009710d108a04a53f860a7c432e8
erasmo.pinheiro <erasmosouza@gmail.com>
2019-11-04 08:36:07 -0300

    Criação do HealthCheck e configuração do Swagger-ui

---
+++ src/main/kotlin/esp/inventory/Health.kt
@@ -0,0 +1,9 @@
+package esp.inventory
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+data class Health(val id: Long, val content: String, val dateNow: LocalDateTime)
+{
+}
---
+++ src/main/kotlin/esp/inventory/HealthController.kt
@@ -0,0 +1,18 @@
+package esp.inventory
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class HealthController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/check")
+    fun check(@RequestParam("name", defaultValue = "World") name: String) =
+        Health(counter.incrementAndGet(), "Hello, $name", LocalDateTime.now())
+}
